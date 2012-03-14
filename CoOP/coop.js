ef7d00951a8ba108e68b5ebd7d4e4aaedf61fca5
Shidhin <shidhincr@gmail.com>
2012-03-14 15:42:03 +0530

    Fixed the notification problem and ContentScript issue with overriding coop functions
    Signed-off-by: Shidhin <shidhincr@gmail.com>

--- CoOP/coop.js
+++ CoOP/coop.js
@@ -3,30 +3,45 @@
 * @Author Shidhin C R
 * @Version 0.0.1
 */
-(function(unreadCountObj,window){
+var _$main$ = function(){
 
-	// copy the original updateUnreadCount function to a variable
-	var origUpdUnRdCnt = unreadCountObj.updateUnreadCount;
 	// request the notification permission --  this is not required because the extension will have the access.
-	//if (window.webkitNotifications.checkPermission() != 0){ window.webkitNotifications.requestPermission() }
+	if (window.webkitNotifications.checkPermission() != 0){ 
+		window.webkitNotifications.requestPermission( final );
+	}else{
+		final();
+	}
+	function final(){
+		// copy the original updateUnreadCount function to a variable
+		var unreadCountObj = window.unreadCount;
+		var origUpdUnRdCnt = window.unreadCount.updateUnreadCount;
+		var lstNotified = '';
+		window.unreadCount.updateUnreadCount = function(){
+			var elementId = ''; 
+			if($('note_' + unreadCountObj.mostRecentlyViewedId) || $('day_entry_' + unreadCountObj.mostRecentlyViewedId)) { 
+				$$('.entry').each(function(element) { 
+					elementId = this._getIdFromEntryElement(element);
+					if(elementId !== this.mostRecentlyViewedId && !this._current_user_entry(element) && elementId > lstNotified) {								
+						var _icon = $('note_'+elementId ).select(".favicon img")[0].getAttribute("src"),
+						_name = $('note_'+elementId ).select(".user_name_text")[0].innerHTML,
+						_content = $('note_'+elementId ).select("#display_text_note_"+elementId)[0].innerHTML +
+						'    ( ' +  $('note_'+elementId ).select(".user_name .meta")[0].innerHTML + ' )';
+						var n = window.webkitNotifications.createNotification(_icon, _name, _content);
+						n.show();
+						lstNotified = elementId;
+					}
+					//setTimeout( function() { n.cancel() }, 10000 );
+				}.bind(unreadCountObj));
+			}
+			// give the control back to the actual function
+			origUpdUnRdCnt.apply(this,arguments)
+		};
+	}
 
-	window.unreadCount.updateUnreadCount = function(){
-		var elementId = ''; 
-		if($('note_' + unreadCountObj.mostRecentlyViewedId) || $('day_entry_' + unreadCountObj.mostRecentlyViewedId)) { 
-			$$('.entry').each(function(element) { 
-				elementId = this._getIdFromEntryElement(element);
-				if(elementId == this.mostRecentlyViewedId) throw $break; 
-				if(this._current_user_entry(element)) return;				
-				var icon = $('note_'+elementId ).select(".favicon img")[0].getAttribute("src")
-				,name = $('note_'+elementId ).select(".user_name_text")[0].innerHTML
-				,content = $('note_'+elementId ).select("#display_text_note_"+elementId)[0].innerHTML +
-				'    ( ' +  $('note_'+elementId ).select(".user_name .meta")[0].innerHTML + ' )' 
-				,n = window.webkitNotifications.createNotification(icon, name, content);
-				n.show();
-				//setTimeout( function() { n.cancel() }, 10000 );
-			}.bind(unreadCountObj));
-		}
-		origUpdUnRdCnt.apply(this,arguments)
-	};
+};
 
-}(unreadCount,window));
+if (!document.xmlVersion) {
+	var script = document.createElement('script');
+	script.appendChild(document.createTextNode('('+ _$main$ +')();'));
+	document.documentElement.appendChild(script);
+}
